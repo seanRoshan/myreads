dd99b0b05ef679b2f48de79b108337bec4398a77
SEAN ROSHAN <sroshan@dentalxchange.com>
2020-03-28 18:25:17 -0700

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Container from "@material-ui/core/Container";
-import FooterComponent from "./components/footer/footer.component";
 
 function App() {
-    return (
-        <Container className="full-width" fixed>
-            <FooterComponent/>
-        </Container>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
