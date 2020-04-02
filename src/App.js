7b6a5eb3279dc09a1b7c0e31e0e9da708169fae0
SEAN ROSHAN <sroshan@dentalxchange.com>
2020-04-02 16:36:04 -0700

    feat: Footer component added
    This component shows copyright text

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import './App.css';
 import Container from "@material-ui/core/Container";
 import FooterComponent from "./components/footer/footer.component";
-import DashboardComponent from "./components/dashboard/dashboard.component";
 
 function App() {
     return (
         <Container className="full-width" fixed>
-            <DashboardComponent/>
             <FooterComponent/>
         </Container>
     );
