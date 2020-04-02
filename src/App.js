b10cf5a4be840b55fa40af0725492c716e3ba82a
SEAN ROSHAN <sroshan@dentalxchange.com>
2020-04-02 16:46:38 -0700

    feat: Dashboard component added
    This component shows the main content on the page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import Container from "@material-ui/core/Container";
 import FooterComponent from "./components/footer/footer.component";
+import DashboardComponent from "./components/dashboard/dashboard.component";
 
 function App() {
     return (
         <Container className="full-width" fixed>
+            <DashboardComponent/>
             <FooterComponent/>
         </Container>
     );
---
+++ src/components/dashboard/dashboard.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './dashboard.component.scss';
+
+const DashboardComponent = () => {
+    return (
+        <main className="dashboard"/>
+    );
+};
+
+export default DashboardComponent;
