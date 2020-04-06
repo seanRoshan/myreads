8f8d86842860e340d559dd18819381e737b6341a
Sean Roshan <sean.roshan.91@gmail.com>
2020-04-06 11:39:07 -0700

    Updates

---
+++ precache-manifest.56abafb1f9ce742448402f45c460927b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "481e08db1e7e04f29efca08fa9c08f6f",
+    "url": "/myreads/index.html"
+  },
+  {
+    "revision": "33e847b1a4a61d981c10",
+    "url": "/myreads/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cbd0e248cb354ab0fbfd",
+    "url": "/myreads/static/js/2.a86db19d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myreads/static/js/2.a86db19d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33e847b1a4a61d981c10",
+    "url": "/myreads/static/js/main.cfbe166a.chunk.js"
+  },
+  {
+    "revision": "1e3fcdb06b277d7016d2",
+    "url": "/myreads/static/js/runtime-main.5a132eca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreads/static/media/logo.5d5d9eef.svg"
+  }
+]);
